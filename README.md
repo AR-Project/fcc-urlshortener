2c201ca8144599474afb524373f2a056a83da1fd
AR Project <alvenresta@gmail.com>
2022-07-29 00:03:01 +0700

    alpha version

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # URL Shortener Microservice
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+This is an alpha version for the URL Shortener Microservice project by AR Project. Boiler plate and requirement for building this project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+
+Known Issues:
+- Every url will generate new short url, even it was duplicate / already generated before.
+- Url validation still include 'localhost' for debug purpose.
+- Data is stored locally. Everytime server is being reset, all short url that already generated will gone.
+
+Plan in the future:
+- Store all data in external database. 
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const database = {};
+
+// URL Validator
+const isValidUrl = (str) => {
+  // Still not sure about regex
+  const regex = /^(https?:\/\/)?(localhost|[\da-z\.-]+\.[a-z\.]{2,6}|[\d\.]+)([\/:?=&#]{1}[\da-z\.-]+)*[\/\?]?/ig
+  if (str.match(regex)) return true;
+  return false;
+}
+
 const app = express();
 
 // Basic Configuration
@@ -10,6 +21,8 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +31,52 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/shorturl/:url?', (req, res) => {
+  // Get short_url from parameters
+  const {url} = req.params;
+  
+  // Check if short_url already in database
+  if (database[url] != undefined) {
+    res.redirect(`${database[url]}`) // Redirect using value of current database url
+  } else {
+    res.send(`invalid short url`); // Invalid short_url
+    res.end();
+  }
+})
+
+app.post('/api/shorturl', (req, res) => {
+  // Parsing body 
+  const { url } = req.body;
+  // console.log(url);
+
+  // Check if url are valid url
+  if (isValidUrl(url)) {
+    // TODO: Check if url already in database, for now, all url will be pushed
+    // into database, no matter it is duplicate.
+
+    // Assign short url
+    const short_url = Object.keys(database).length + 1;
+
+    // Add url into database
+    database[short_url] = url;
+    // TODO: Store url in database
+
+    // Wrap a respond if url is valid
+    const wrapper = {
+      original_url: url,
+      short_url: short_url,
+    }
+
+    // Send wrap into json
+    res.json(wrapper);
+    res.end();
+  } else {
+    res.json({
+      error: "invalid url"
+    })
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
